1c1817b54be85018acac6b6db9c8582c6d344f5c
Chi Po Choi <cpchoi@ucdavis.edu>
2018-07-30 19:38:40 -0700

    csne test

---
+++ test/csne.jl
@@ -0,0 +1,29 @@
+@testset "csne" begin
+
+    @testset "β = 0" begin
+        m = 100
+        A = randn(m,0)
+        R = Array{Float64}(undef, 0,0)
+
+        for i in 1:m
+            a = randn(m)
+            R = qraddcol(A, R, a)
+            A = [A a]
+            @test  norm( R'*R - A'*A ) ≈ 0.0 atol=1e-5
+        end
+    end
+
+    @testset "β > 0" begin
+        m = 100
+        A = randn(m,0)
+        R = Array{Float64}(undef, 0,0)
+        β = 0.1
+        for i in 1:m
+            a = randn(m)
+            B = [A a]
+            R = qraddcol(A, R, a, β)
+            @test norm( R'*R - B'*B - β^2*I ) ≈ 0.0 atol=1e-5
+        end
+    end
+
+end
